55e9d865a7ae747c553c8e1e88d27cebb256e24e
rahulajudiya <rahulajudiya9@gmail.com>
2024-12-30 12:14:40 +0530

    befor

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import ok1 from '../src/asset/image/pexels-anjana-c-169994-674010.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='ok1'>
+        <div className='ok1-img'>
+          <img src={ok1} />
+        </div>
+        <div className='text'>
+          <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
