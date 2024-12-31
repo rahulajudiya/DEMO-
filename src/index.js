de897c387e310302dd43e585e2929c9af8f4d16a
prem-kavathiya <premkavathiya35@gmail.com>
2024-12-31 18:39:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,4 +14,25 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/reportWebVitals();
+
+
+let arr = ["prem","raj","hinali","rahul","parthiv","prince","manshavi","meet"]
+
+// let ok=arr.filter(item=>item.includes("hin"))
+// console.log(ok)
+
+// let arrToString = arr.toString()
+// console.log(arrToString)
+
+// let arrJoin = arr.join("**")
+// ^=>"jenathi object join karva hoy te pass karvanu hoy"
+// console.log(arrJoin)
+
+// let arrAt = arr.at(3)
+// // ^=>"index pass karvani hoy"
+// console.log(arrAt)
+
+// let arrPop = arr.pop()
+// ^=> ""
+// console.log(arrPop)
